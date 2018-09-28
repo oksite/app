f357d084d00d7392eb775f078637660c8ddac9fb
oksite <wangdexiang@haohaoce.com>
2018-09-28 11:16:21 +0800

    app 全局配置文件

---
+++ app.js
@@ -0,0 +1,34 @@
+/**
+ * @access 自定义入口 涉及全局服务等
+ * @author by oksite
+ * @emits beforeStart 中不建议做太耗时的操作，框架会有启动的超时检测
+ */
+
+ // app.js
+module.exports = app => {
+  
+  app.beforeStart(async () => {
+
+
+    // 应用会等待这个函数执行完成才启动
+    // app.cities = await app.curl('http://example.com/city.json', {
+    //   method: 'GET',
+    //   dataType: 'json',
+    // });
+
+
+    // 也可以通过以下方式来调用 Service
+    // const ctx = app.createAnonymousContext();
+    // app.cities = await ctx.service.cities.load();
+
+
+    //Controller 调用直接this.ctx.app.cities
+
+
+
+
+  });
+};
+
+
+
--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,6 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
 
   async index (){
-    var bufs = [];
     const ctx = this.ctx;
     const { serverUrl } = this.config.path;   
     const result = await ctx.curl(`${serverUrl}/topics??page=1&limit=5&mdrender=false&tab=share`);
